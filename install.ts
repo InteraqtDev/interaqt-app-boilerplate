185e3a8a1817876eeb38f3dba2e2fd847de26513
sskyy <skyking_H@hotmail.com>
2023-12-12 22:37:20 +0800

    fix: install script

--- install.ts
+++ install.ts
@@ -1,11 +1,13 @@
 import {Controller, MonoSystem, SQLiteDB} from "@interaqt/runtime";
 import {entities, interactions, relations, states, activities} from './app/index.js'
 import {DATABASE_ADDR} from "./config.js";
-import { unlinkSync } from "fs";
-
+import { existsSync } from "fs";
+import chalk from "chalk";
 
 try {
-    await unlinkSync(DATABASE_ADDR)
+    if (existsSync(DATABASE_ADDR)) {
+        console.log(chalk.red(`${DATABASE_ADDR} already exist, remove it before install`))
+    }
     const db = new SQLiteDB(DATABASE_ADDR)
     const system = new MonoSystem(db)
     const controller = new Controller(system, entities, relations, activities, interactions, states)
